49e4cfebb9d2d6a4a3afeca3732d1cb2075e980e
Yuan He <lendage@gmail.com>
2012-02-17 22:55:40 +0800

    Update cursor:wait to cursor:none

--- doom.js
+++ doom.js
@@ -1,7 +1,7 @@
 (function(){
   var head = document.getElementsByTagName('head')[0];
   var style = document.createElement('style');
-  var rules = document.createTextNode('*::selection { background: transparent!important;   } *::-moz-selection { background: transparent!important; } * { cursor: wait!important; ');
+  var rules = document.createTextNode('*::selection { background: transparent!important;   } *::-moz-selection { background: transparent!important; } * { cursor: none!important; ');
   style.type = 'text/css';
   if(style.styleSheet)
     style.styleSheet.cssText = rules.nodeValue;
