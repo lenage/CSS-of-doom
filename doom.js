a53768478f5c11bcaf253bac548bb4239f3f3ac2
Peter-Jakob Cosoroaba <pjcosoroaba@lucernus.(none)>
2012-02-17 10:12:13 +0200

    bookmarklet

---
+++ doom.js
@@ -0,0 +1,9 @@
+(function(){
+  if (!document.getElementById('css-of-doom')){
+    var objHead = document.getElementsByTagName('head'); 
+    var objCSS = objHead[0].appendChild(document.createElement('link')); 
+    objCSS.id = 'css-of-doom'; 
+    objCSS.rel = 'stylesheet'; 
+    objCSS.href = 'https://raw.github.com/midday/CSS-of-doom/master/doom.css'; 
+    objCSS.type = 'text/css';    
+ })()
